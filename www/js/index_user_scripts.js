1e44f06647fe2cd5391509c64fff72309b02d5c6
Kemel Zaidan <kemelzaidan@gmail.com>
2014-03-30 23:07:51 -0300

    getting quotation and displaying at the proper place

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -6,17 +6,27 @@
  */
  function register_event_handlers()
  {
-    
+        // user preferences that later will be read from a preferences.json
         var btc_quotation = 628.88;
+        var currency = "BRL/";
+        var globalBTC = false;
+        var apiURL = "https://api.bitcoinaverage.com/ticker/";
      
+        
+     
+        // toggol the money mask on money_field class 
         $('.money_field').maskMoney();
+        
+        // show the convension forms and hide the final result divs 
+        // convert to fiat money button
         $("#to_fiat_button").click(function(evt)
         {
             $('#btc2fiat').show();
             $('#fiat2btc').hide();
             $('.final_block').hide();
         });
-        
+     
+        // convert to BTC button
         $("#to_btc_button").click(function(evt)
         {
             $('#btc2fiat').hide();
@@ -24,7 +34,10 @@
             $('.final_block').hide();
         });
      
-     // see https://en.bitcoin.it/wiki/FAQ#What_do_I_call_the_various_denominations_of_bitcoin.3F for units definition
+        /* function to convert fiat money to BTC
+        to understand the basic BTC sub-units, please see:
+        https://en.bitcoin.it/wiki/FAQ#What_do_I_call_the_various_denominations_of_bitcoin.3F for units definition
+        */
         $("#convert2btc_button").click(function(evt)
         {
             var amount = numeral($('#money_amount').val());
@@ -37,7 +50,8 @@
             $('#satoshi_value').html(result.multiply(100000000).value()); // 8 zeros
             $('#final_btc_block').show();
         });
-     
+        
+        // function to convert BTC to fiat money
         $("#convert2fiat_button").click(function(evt)
         {
             var amount = numeral($('#btc_amount').val());
@@ -64,6 +78,35 @@
            // $('#final_money_value').html(result.value());
             alert(result.value());
 //            $('#final_btc_block').show();
+        });
+        
+        // grab BTC quotation function
+        $("#update_btc_button").click(function(evt)
+        {
+            // decide if we're going to request a global quotation or market specific
+            var quotationAPI = "";
+            if (globalBTC === true){
+                quotationAPI = apiURL + "global/" + currency;
+            }
+            else {
+                quotationAPI = apiURL + currency;
+            } 
+            
+            // quotation API request
+            var quotation;
+            
+            $.getJSON( quotationAPI, function(json){
+                console.log("successfully connected to " + quotationAPI);
+                quotation = json;
+                btc_quotation = quotation["24h_avg"];
+            })
+                .done(function(){
+                    console.log("finished requesting API JSON");
+                    $("#quotation_display").html(btc_quotation);
+                })
+                .fail(function(){
+                    console.log("error on JSON request");
+                })
         });
 }
  $(document).ready(register_event_handlers);
